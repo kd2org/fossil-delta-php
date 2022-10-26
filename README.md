61bde2cbb184f60a759cfa8e40494828d2b9aff9
bohwaz <github.bohwaz@miam.kd2.org>
2022-10-27 00:35:03 +0200

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# fossil-delta-php
-PHP implementation of Fossil delta algorithm
+# PHP implementation of Fossil delta algorithm
+
+Ported from the C code of Fossil SCM.
+
+See delta details here:
+
+ * http://www.fossil-scm.org/xfer/doc/trunk/www/delta_format.wiki
+ * http://www.fossil-scm.org/xfer/doc/trunk/www/delta_encoder_algorithm.wiki
+
+## Author
+
+2013 - KD2.org/BohwaZ <https://bohwaz.net/>
+
+## Other Implementations
+
+* [haXe](https://lib.haxe.org/p/fossil-delta)
+* [Python](https://pypi.org/project/python-fossil-delta/)
+* [C#](https://github.com/endel/FossilDelta)
+* [Javascript](https://github.com/dchest/fossil-delta-js) ([Demo](https://dchest.github.io/fossil-delta-js/))
+* [Rust](https://crates.io/crates/fossil-delta)
--- test.php
+++ test.php
@@ -18,12 +18,16 @@ $d = file_get_contents(__DIR__ . '/testdata/small.delta');
 test_assert($d == $delta->create($orig, $target), 'small: delta created differs from reference sample');
 test_assert($target == $delta->apply($orig, $d), 'small: target created from delta differs from target sample file');
 
+echo '.';
+
 $orig = file_get_contents(__DIR__ . '/testdata/code.orig');
 $target = file_get_contents(__DIR__ . '/testdata/code.target');
 $d = file_get_contents(__DIR__ . '/testdata/code.delta');
 
 test_assert($d == $delta->create($orig, $target), 'code: delta created differs from reference sample');
 test_assert($target == $delta->apply($orig, $d), 'code: target created from delta differs from target sample file');
+
+echo '.';
 
 $orig = file_get_contents(__DIR__ . '/testdata/binary.orig');
 $target = file_get_contents(__DIR__ . '/testdata/binary.target');
